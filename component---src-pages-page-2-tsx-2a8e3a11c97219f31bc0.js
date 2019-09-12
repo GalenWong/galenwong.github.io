44b93eb432480bd183b107e484bda57cb005823f
GalenWong <wonggalen1999@gmail.com>
2019-09-12 18:10:12 +0800

    Updates

---
+++ component---src-pages-page-2-tsx-2a8e3a11c97219f31bc0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{272:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),c=n(88);t.default=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-2a8e3a11c97219f31bc0.js.map
---
+++ styles-4ebc1ca2a1ebeff9c4e3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{227:function(n,o,w){},228:function(n,o,w){},229:function(n,o,w){}}]);
+//# sourceMappingURL=styles-4ebc1ca2a1ebeff9c4e3.js.map
