084cfcc1f70f0b7be5d5edbc54d43667385bd635
GalenWong <wonggalen1999@gmail.com>
2019-09-12 13:12:10 +0800

    Created Logo and changes metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`
+		title: `In the Middle 站中間`,
+		description: `Personal Blog by Galen Wong`,
+		author: `Galen Wong`
 	},
 	plugins: [
 		`gatsby-plugin-material-ui`,
@@ -79,13 +79,13 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `In the Middle 站中間`,
+				short_name: `In the Middle`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				// background_color: `#663399`,
+				// theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+				icon: `src/images/logo.svg` // This path is relative to the root of the site.
 			}
 		}
 		// this (optional) plugin enables Progressive Web App + Offline functionality
