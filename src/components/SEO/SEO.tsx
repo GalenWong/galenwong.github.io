48c24154335843fbfa701f7f8669550a99007f3f
GalenWong <wonggalen1999@gmail.com>
2019-09-17 10:38:38 +0800

    Added social media banner

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import bannerURL from './banner.png';
+
 interface SEOProps {
 	title?: string;
 	description?: string;
@@ -14,7 +18,7 @@ interface SEOProps {
 function SEO({
 	title,
 	description,
-	// image,
+	image,
 	pathname,
 	article = false
 }: SEOProps) {
@@ -35,12 +39,15 @@ function SEO({
 		title: title || siteMetadata.title,
 		titleTemplate: title ? siteMetadata.titleTemplate : undefined,
 		description: description || siteMetadata.description,
-		url: `${siteMetadata.url}${pathname || '/'}`
+		url: `${siteMetadata.url}${pathname || '/'}`,
+		image: `${siteMetadata.url}${image || bannerURL}`
 	};
 	return (
 		<Helmet title={seo.title} titleTemplate={seo.titleTemplate}>
-			<meta name="description" content={seo.description}/>
+			<meta name="description" content={seo.description} />
+			<meta name="image" content={seo.image} />
 			{seo.url && <meta property="og:url" content={seo.url} />}
+			<meta property="og:image" content={seo.image} />
 			{(article ? true : null) &&
 				<meta property="og:type" content="article" />
 			}
