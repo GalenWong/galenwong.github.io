652e7d7ea7ffbd8f4dc942444c431bdf25f3d4c0
GalenWong <wonggalen1999@gmail.com>
2020-08-10 22:11:57 +0800

    Updates

---
+++ component---src-pages-404-tsx-6b4b131df8374f92243f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t);var o=n("q1tI"),a=n.n(o),l=n("4XLq"),i=n("Wbzz");t.default=function(){return a.a.createElement(l.a,null,a.a.createElement("h3",null," This page does not exist in the middle. "),a.a.createElement("p",null,a.a.createElement(i.Link,{to:"/"},'Go back to "the middle"'),", before you get too lost…"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-6b4b131df8374f92243f.js.map
