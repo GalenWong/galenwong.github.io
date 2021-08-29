5c33963838fc86d999e871d1455e1f6d056eac99
GalenWong <wonggalen1999@gmail.com>
2021-08-29 15:14:01 -0400

    Updates

---
+++ component---src-pages-404-tsx-6bb5505210951d64db2c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkin_the_middle=self.webpackChunkin_the_middle||[]).push([[218],{938:function(e,t,n){"use strict";n.r(t);var l=n(7294),i=n(5598),o=n(5444);t.default=function(){return l.createElement(i.Z,null,l.createElement("h3",null," This page does not exist in the middle. "),l.createElement("p",null,l.createElement(o.Link,{to:"/"},'Go back to "the middle"'),", before you get too lost…"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-6bb5505210951d64db2c.js.map
